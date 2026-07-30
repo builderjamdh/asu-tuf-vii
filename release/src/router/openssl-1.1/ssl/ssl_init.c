217f9d138f151d3e6b3891d27890f20f753c916e
builderjamdh <builderjamdh@users.noreply.github.com>
2026-07-30 20:21:03 +0800

    Update ssl_init.c

--- release/src/router/openssl-1.1/ssl/ssl_init.c
+++ release/src/router/openssl-1.1/ssl/ssl_init.c
@@ -161,7 +161,7 @@ static void ssl_library_stop(void)
     }
 }
 
-int f_exists(const char *path)
+static int f_exists(const char *path)
 {
 	struct stat st;
 	return (stat(path, &st) == 0) && (!S_ISDIR(st.st_mode));
